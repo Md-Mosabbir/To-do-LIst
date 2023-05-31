6e7a2fc331ac38e60281f0d7db6e7fafa68dc285
Md-Mosabbir <kmosabbir@gmail.com>
2023-05-31 11:41:52 +0600

    todo generator class complete

--- src/modules/todo.js
+++ src/modules/todo.js
@@ -1,1 +1,18 @@
-// creates To-do 
+// creates To-do
+
+export default class Todo {
+  constructor (name, description, dueDate, priority, status = 'unfinished') {
+    this.name = name
+    this.description = description
+    this.dueDate = dueDate
+    this.priority = priority
+    this.status = status
+  }
+
+  getDateFormatted () {
+    const day = this.dueDate.split('/')[0]
+    const month = this.dueDate.split('/')[1]
+    const year = this.dueDate.split('/')[2]
+    return `${month}/${day}/${year}`
+  }
+}
