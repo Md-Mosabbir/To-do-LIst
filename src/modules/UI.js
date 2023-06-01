87246861a3b8be7a6fc3e542a593359b9bfa188a
Md-Mosabbir <kmosabbir@gmail.com>
2023-06-01 07:36:07 +0600

    userinterface() called

--- src/modules/UI.js
+++ src/modules/UI.js
@@ -2,20 +2,5 @@ import Todo from './todo'
 import { Tabs, Project } from './tab'
 
 export function userInterface () {
-  const addingButton = document.createElement('button')
-  addingButton.textContent = 'Hi'
 
-  document.querySelector('body').appendChild(addingButton)
-
-  addingButton.addEventListener('click', (e) => {
-    e.target = questions()
-  })
-
-  function questions () {
-    const name = prompt('name')
-    const game = prompt('name')
-    const shame = prompt('name')
-    const blame = prompt('name')
-    const work = new Todo(name, game, shame, blame)
-  }
 }
