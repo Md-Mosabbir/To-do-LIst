2b19ca35b710dbdaab28ea8f491792edf2d23cbf
Md-Mosabbir <kmosabbir@gmail.com>
2023-06-01 06:56:34 +0600

    tabs have funtional Tabs and Project

--- src/modules/tab.js
+++ src/modules/tab.js
@@ -1,6 +1,22 @@
 export class Tabs {
-  constructor (name, array = []) {
-    this.name = name
-    this.array = array
+  static _array = []
+
+  static addTask (obj) {
+    this._array.push(obj)
+    console.log(this._array)
+  }
+}
+
+export class Project {
+  static _projectArray = []
+
+  static addingProjects () {
+    const _projects = []
+    this._projectArray.push(_projects)
+    return _projects
+  }
+
+  static addingTask (_projects, obj) {
+    _projects.push(obj)
   }
 }
