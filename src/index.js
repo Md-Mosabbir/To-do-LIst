2da0937564e683b9626736b6dcfd4004f23e71c6
Md-Mosabbir <kmosabbir@gmail.com>
2023-06-09 21:16:00 +0600

    active class pending

--- src/index.js
+++ src/index.js
@@ -1,24 +1,39 @@
 import './style.css'
 import '@fortawesome/fontawesome-free/js/all.js'
 
-import { UserInterface } from './modules/UI'
+import UserInterface from './modules/UI'
+import { inboxStorage } from './modules/tab'
 
 // eslint-disable-next-line no-unused-vars
 export const inboxContainer = (function () {
-  const inbox = document.getElementById('adding-button')
+  const addingTaskButton = document.getElementById('adding-button')
 
-  inbox.addEventListener('click', (e) => {
+  addingTaskButton.addEventListener('click', (e) => {
     e.stopPropagation()
     // eslint-disable-next-line no-unused-vars
     const ui = new UserInterface() // Create an instance of UserInterface
-    inbox.style.display = 'none'
+    addingTaskButton.style.display = 'none'
   })
   return {
     displayBlock: function () {
-      inbox.style.display = 'block'
+      addingTaskButton.style.display = 'block'
     },
     displayNone: function () {
-      inbox.style.display = 'none'
+      addingTaskButton.style.display = 'none'
+    }
+  }
+})()
+
+export const activeClass = (function () {
+  const inboxTaskNumber = document.querySelector('.notification-number')
+
+  const inboxTab = document.querySelector('.inbox')
+  inboxTab.addEventListener('click', () => {
+    console.log('hi')
+  })
+  return {
+    updateNumberOfInboxNotification: function () {
+      inboxTaskNumber.textContent = inboxStorage.getTask().length
     }
   }
 })()
