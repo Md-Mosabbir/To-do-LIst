eb2da285b24361d608ffd4343cd4cb8faf2f2280
Md-Mosabbir <kmosabbir@gmail.com>
2023-06-21 20:24:48 +0600

    Created a way to switch tabs and show add button

--- src/modules/tab-management.js
+++ src/modules/tab-management.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-new */
 /* eslint-disable no-plusplus */
 /* eslint-disable import/no-cycle */
 /* eslint-disable no-return-assign */
@@ -8,184 +9,162 @@
 /* eslint-disable import/prefer-default-export */
 import { inboxStorage, projectStorage } from './tab'
 
-import { DisplayTask } from './UI'
+import { DisplayTask, addingButton } from './UI'
 
 // manage active class holder projects switching and creation
 
+export const trackingActiveClass = (() => {
+  const inboxTab = document.querySelector('.inbox')
+  const project = document.querySelector('.list-and-tasks-container')
 
-export const  trackingActiveClass = (() => {
-    const inboxTab = document.querySelector('.inbox')
-    const project = document.querySelector('.list-and-tasks-container')
-
-    function removeActiveClass () {
-        inboxTab.classList.remove('active')
-        const buttons = project.querySelectorAll('.set-active')
-        buttons.forEach((button) => {
-          button.classList.remove('active')
-        })
-    }
-    inboxTab.addEventListener('click', () => {
-        removeActiveClass()
-        inboxTab.classList.add('active')
-        new DisplayTask(inboxStorage.getTasks()).displayTasks()
-        project.classList.remove('c-active')
-        
-
+  function removeActiveClass() {
+    inboxTab.classList.remove('active')
+    const buttons = project.querySelectorAll('.set-active')
+    buttons.forEach((button) => {
+      button.classList.remove('active')
+      addingButton.style.display = 'block'
     })
-    project.addEventListener('click', (e) => {
-        if (e.target.classList.contains('set-active')){
-            removeActiveClass()
-            // Add the "Active" class to the clicked button
-            e.target.classList.add('active')
-            project.classList.add('c-active')
-            new DisplayTask(projectStorage.getTaskToProject(trackingActiveClass.getIndexOfActiveTodo(),trackingActiveClass.getButtonIndex())).displayTasks()
-
-        }
-
-    })
-
-
-    return {
-        inboxContainingActive() {
-            return inboxTab.classList.contains('active')
-        },
-        projectContainingActive () {
-            return project.classList.contains('c-active')
-        },
-        getIndexOfActiveTodo () {
-            const lists = document.querySelectorAll('.list');
-            let listIndex = -1;
-          
-            for (let i = 0; i < lists.length; i++) {
-              if (lists[i].querySelector('.set-active.active')) {
-                listIndex = i;
-                break;
-              }
-            }
-          
-            return listIndex
-
-            
-        },
-        getButtonIndex() {
-            const lists = document.querySelectorAll('.list');
-          
-            for (let i = 0; i < lists.length; i++) {
-              const activeButton = lists[i].querySelector('.task-of-proj.set-active.active');
-              if (activeButton) {
-                const buttonIndex = Array.from(lists[i].querySelectorAll('.task-of-proj')).indexOf(activeButton);
-                return buttonIndex
-                 // Exit the loop after finding the active button
-              }
-              
-            }
-            
-        }
-        
-          
+  }
+  inboxTab.addEventListener('click', () => {
+    removeActiveClass()
+    inboxTab.classList.add('active')
+    new DisplayTask(inboxStorage.getTasks())
+    project.classList.remove('c-active')
+  })
+  project.addEventListener('click', (e) => {
+    if (e.target.classList.contains('set-active')) {
+      removeActiveClass()
+      // Add the "Active" class to the clicked button
+      e.target.classList.add('active')
+      project.classList.add('c-active')
+      new DisplayTask(
+        projectStorage.getTaskToProject(
+          trackingActiveClass.getIndexOfActiveTodo(),
+          trackingActiveClass.getButtonIndex()
+        )
+      )
     }
+  })
 
+  return {
+    inboxContainingActive() {
+      return inboxTab.classList.contains('active')
+    },
+    projectContainingActive() {
+      return project.classList.contains('c-active')
+    },
+    getIndexOfActiveTodo() {
+      const lists = document.querySelectorAll('.list')
+      let listIndex = -1
+
+      for (let i = 0; i < lists.length; i++) {
+        if (lists[i].querySelector('.set-active.active')) {
+          listIndex = i
+          break
+        }
+      }
+
+      return listIndex
+    },
+    getButtonIndex() {
+      const lists = document.querySelectorAll('.list')
+
+      for (let i = 0; i < lists.length; i++) {
+        const activeButton = lists[i].querySelector(
+          '.task-of-proj.set-active.active'
+        )
+        if (activeButton) {
+          const buttonIndex = Array.from(
+            lists[i].querySelectorAll('.task-of-proj')
+          ).indexOf(activeButton)
+          return buttonIndex
+          // Exit the loop after finding the active button
+        }
+      }
+    },
+  }
 })()
 
-
-
-
 export class ProjectCreation {
-    constructor () {
+  constructor() {
+    this.projectList = document.createElement('div')
+    this.projectList.classList.add('list')
 
-        this.projectList = document.createElement('div')
-        this.projectList.classList.add('list')
+    this.projectNav = document.createElement('div')
+    this.projectNav.classList.add('project-nav')
+    this.titleH3 = document.createElement('input')
+    this.doneButton = document.createElement('button')
+    this.doneButton.textContent = 'Add'
 
-        this.projectNav = document.createElement('div')
-        this.projectNav.classList.add('project-nav')
-        this.titleH3 = document.createElement('input')
-        this.doneButton = document.createElement('button')
-        this.doneButton.textContent = 'Add'
+    this.displaySpan = document.createElement('h3')
+    this.displaySpan.style.display = 'none'
 
-        
+    this.addProjectTask = document.createElement('button')
+    this.addProjectTask.classList.add('add-projects')
+    this.addProjectTask.textContent = '+'
+    this.addProjectTask.style.display = 'none'
 
-        
-        this.displaySpan = document.createElement('h3')
-        this.displaySpan.style.display = 'none'
-        
-        this.addProjectTask = document.createElement('button')
-        this.addProjectTask.classList.add('add-projects')
-        this.addProjectTask.textContent = '+'
-        this.addProjectTask.style.display = 'none'
+    this.projectNav.appendChild(this.titleH3)
+    this.projectNav.appendChild(this.doneButton)
+    this.projectNav.appendChild(this.displaySpan)
+    this.projectNav.appendChild(this.addProjectTask)
+    this.projectList.appendChild(this.projectNav)
 
-        this.projectNav.appendChild(this.titleH3)
-        this.projectNav.appendChild(this.doneButton)
-        this.projectNav.appendChild(this.displaySpan)
-        this.projectNav.appendChild(this.addProjectTask)
-        this.projectList.appendChild(this.projectNav)
+    document
+      .querySelector('.list-and-tasks-container')
+      .appendChild(this.projectList)
+    this.doneButton.addEventListener('click', this.submitProject.bind(this))
+    this.addProjectTask.addEventListener(
+      'click',
+      this.createTaskToProject.bind(this)
+    )
+  }
 
-        document.querySelector('.list-and-tasks-container').appendChild(this.projectList)
-        this.doneButton.addEventListener('click', this.submitProject.bind(this))
-        this.addProjectTask.addEventListener('click', this.createTaskToProject.bind(this))
-        
-    }
+  submitProject() {
+    this.inputValue = this.titleH3.value
+    this.displaySpan.textContent = `${this.inputValue}`
+    this.titleH3.style.display = 'none'
+    this.doneButton.style.display = 'none'
 
-    submitProject () {
-        this.inputValue = this.titleH3.value
-        this.displaySpan.textContent = `${this.inputValue}`
-        this.titleH3.style.display = 'none'
-        this.doneButton.style.display = 'none'
+    this.displaySpan.style.display = 'inline-block'
+    this.addProjectTask.style.display = 'inline-block'
+    projectStorage.addProject()
+  }
 
-        this.displaySpan.style.display = 'inline-block'
-        this.addProjectTask.style.display = 'inline-block'
-        projectStorage.addProject()
+  createTaskToProject() {
+    this.taskOfProjectInputContainer = document.createElement('div')
+    this.taskOfProjectInput = document.createElement('input')
+    this.doneButton = document.createElement('button')
+    this.doneButton.textContent = 'Add'
 
+    this.doneButton.addEventListener(
+      'click',
+      this.submitTaskToProject.bind(this)
+    )
 
+    this.taskOfButton = document.createElement('button')
+    this.taskOfButton.style.display = 'none'
+    this.taskOfButton.classList.add('task-of-proj')
+    this.taskOfButton.classList.add('set-active')
 
+    this.taskOfProjectInputContainer.appendChild(this.taskOfProjectInput)
+    this.taskOfProjectInputContainer.appendChild(this.doneButton)
+    this.taskOfProjectInputContainer.appendChild(this.taskOfButton)
+    this.projectList.appendChild(this.taskOfProjectInputContainer)
+  }
 
-    }
-
-    createTaskToProject () {
-
-        this.taskOfProjectInputContainer = document.createElement('div')
-        this.taskOfProjectInput = document.createElement('input')
-        this.doneButton = document.createElement('button')
-        this.doneButton.textContent = 'Add'
-
-
-        this.doneButton.addEventListener('click', this.submitTaskToProject.bind(this))
-        
-
-        this.taskOfButton = document.createElement('button')
-        this.taskOfButton.style.display = 'none'
-        this.taskOfButton.classList.add('task-of-proj')
-        this.taskOfButton.classList.add('set-active')
-
-        
-        this.taskOfProjectInputContainer.appendChild(this.taskOfProjectInput)
-        this.taskOfProjectInputContainer.appendChild(this.doneButton)
-        this.taskOfProjectInputContainer.appendChild(this.taskOfButton)
-        this.projectList.appendChild(this.taskOfProjectInputContainer)
-        
-        
-
-       
-
-        
-
-
-
-    }
-
-    submitTaskToProject () {
-        this.taskOfButton.textContent =  this.taskOfProjectInput.value
-        this.taskOfButton.style.display = 'block'
-        this.taskOfProjectInput.style.display = 'none'
-        this.doneButton.style.display = 'none'
-        const getList = document.querySelectorAll('.list')
-        projectStorage.addTodoToProject(Array.from(getList).indexOf(this.projectList))
-
-       
-        
-
-    }
-
+  submitTaskToProject() {
+    this.taskOfButton.textContent = this.taskOfProjectInput.value
+    this.taskOfButton.style.display = 'block'
+    this.taskOfProjectInput.style.display = 'none'
+    this.doneButton.style.display = 'none'
+    const getList = document.querySelectorAll('.list')
+    projectStorage.addTodoToProject(
+      Array.from(getList).indexOf(this.projectList)
+    )
+  }
 }
 
-document.querySelector('#adding-project').addEventListener('click', () => new ProjectCreation)
-
+document
+  .querySelector('#adding-project')
+  .addEventListener('click', () => new ProjectCreation())
