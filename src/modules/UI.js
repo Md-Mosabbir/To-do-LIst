3f46cd8fb461e9d8c57f94b9cb1b93adbd59f494
Md-Mosabbir <kmosabbir@gmail.com>
2023-06-22 17:40:30 +0600

    statusElement can check if it is checked

--- src/modules/UI.js
+++ src/modules/UI.js
@@ -1,3 +1,6 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable consistent-return */
+/* eslint-disable no-new */
 /* eslint-disable import/no-cycle */
 /* eslint-disable no-use-before-define */
 /* eslint-disable no-underscore-dangle */
@@ -8,9 +11,10 @@
 import Todo from './todo'
 import { trackingActiveClass } from './tab-management'
 import { inboxStorage, projectStorage } from './tab'
+import { addingButton } from '..'
 
 export class CreateTaskInput {
-  constructor () {
+  constructor() {
     this.containerDiv = document.createElement('div')
     this.createCheckbox()
     this.createName()
@@ -20,27 +24,20 @@ export class CreateTaskInput {
     this.submitButton()
     this.cancelButton()
 
-    
-    
-    
-    
     document.querySelector('.task').appendChild(this.containerDiv)
-
   }
 
   createCheckbox() {
     this.checkbox = document.createElement('input')
-    this.checkbox.type = 'checkbox';
+    this.checkbox.type = 'checkbox'
     this.checkbox.name = 'checkbox'
     this.checkbox.style.display = 'none'
     this.containerDiv.appendChild(this.checkbox)
 
     return this.checkbox
-    
   }
 
-
-  createName(){
+  createName() {
     this.inputFieldName = document.createElement('input')
     this.inputFieldName.type = 'text'
     this.inputFieldName.placeholder = 'name'
@@ -48,10 +45,9 @@ export class CreateTaskInput {
     this.inputFieldName.id = 'name'
     this.containerDiv.appendChild(this.inputFieldName)
     return this.inputFieldName
-
   }
 
-  createDescription(){
+  createDescription() {
     this.inputFieldDesc = document.createElement('input')
     this.inputFieldDesc.type = 'text'
     this.inputFieldDesc.placeholder = 'description'
@@ -59,10 +55,9 @@ export class CreateTaskInput {
     this.inputFieldDesc.id = 'description'
     this.containerDiv.appendChild(this.inputFieldDesc)
     return this.inputFieldDesc
-
   }
 
-  createDueDate (){
+  createDueDate() {
     this.inputDate = document.createElement('input')
     this.inputDate.type = 'text'
     this.inputDate.id = 'DueDate'
@@ -70,46 +65,42 @@ export class CreateTaskInput {
     this.containerDiv.appendChild(this.inputDate)
     return this.inputDate
   }
-  
-  createPriority () {
-   this.dropDown = document.createElement('select')
-   this.dropDown.setAttribute('id', 'priorityDropdown')
-   
-   this.options = ['Low', 'Medium', 'High']
-   
-   for (let index = 0; index < this.options.length; index++) {
-    const option = document.createElement('option')
-    option.textContent = this.options[index]
-    this.dropDown.appendChild(option)
-    
-   }
-   this.containerDiv.appendChild(this.dropDown)
-   return this.dropDown
+
+  createPriority() {
+    this.dropDown = document.createElement('select')
+    this.dropDown.setAttribute('id', 'priorityDropdown')
+
+    this.options = ['Low', 'Medium', 'High']
+
+    for (let index = 0; index < this.options.length; index++) {
+      const option = document.createElement('option')
+      option.textContent = this.options[index]
+      this.dropDown.appendChild(option)
+    }
+    this.containerDiv.appendChild(this.dropDown)
+    return this.dropDown
   }
 
-  submitButton () {
+  submitButton() {
     this.submit = document.createElement('button')
     this.submit.id = 'submit'
     this.submit.textContent = 'Submit'
-    this.submit.addEventListener('click', () => this.handleSubmit() ) // later
+    this.submit.addEventListener('click', () => this.handleSubmit()) // later
     this.containerDiv.appendChild(this.submit)
 
     return this.submit
   }
 
-  cancelButton () {
+  cancelButton() {
     this.cancel = document.createElement('button')
     this.cancel.id = 'cancel'
     this.cancel.textContent = 'Cancel'
     this.cancel.addEventListener('click', () => {
       this.removeContainer()
-      
-    } )
+    })
     this.containerDiv.appendChild(this.cancel)
-    
 
     return this.cancel
-    
   }
 
   removeContainer() {
@@ -117,132 +108,132 @@ export class CreateTaskInput {
     addingButton.style.display = 'block'
   }
 
-  getValue (id) {
+  getValue(id) {
     return id.value
-
-  
   }
-  
+
+  getInputFiledsFilled() {
+    if (
+      this.getValue(this.inputFieldName) !== '' &&
+      this.getValue(this.inputDate) !== ''
+    ) {
+      return true
+    }
+  }
+
+  displayTask(task) {
+    // eslint-disable-next-line no-unused-vars
+    const display = new DisplayTask(task)
+  }
+
   handleSubmit() {
     // check active class holder
 
     // add accordingly
-    if (trackingActiveClass.inboxContainingActive() === true){
-      if (this.getValue(this.inputFieldName )!== '' && this.getValue(this.inputDate) !== '') {
+    if (trackingActiveClass.inboxContainingActive()) {
+      if (this.getInputFiledsFilled()) {
         const name = this.getValue(this.inputFieldName)
         const description = this.getValue(this.inputFieldDesc)
         const dueDate = this.getValue(this.inputDate)
         const priority = this.getValue(this.dropDown)
         const status = this.checkbox.checked
 
-
         const _task = new Todo(name, description, dueDate, priority, status)
         inboxStorage.addTask(_task)
-        console.log(inboxStorage.getTasks())
 
-
-        new DisplayTask(inboxStorage.getTasks()).displayTasks()
+        this.displayTask(inboxStorage.getTasks())
 
         this.containerDiv.style.display = 'none'
         addingButton.style.display = 'block'
-        
       }
+    } else if (trackingActiveClass.projectContainingActive()) {
+      if (
+        this.getValue(this.inputFieldName) !== '' &&
+        this.getValue(this.inputDate) !== ''
+      ) {
+        const name = this.getValue(this.inputFieldName)
+        const description = this.getValue(this.inputFieldDesc)
+        const dueDate = this.getValue(this.inputDate)
+        const priority = this.getValue(this.dropDown)
+        const status = this.checkbox.checked
 
-    }
-    else if (trackingActiveClass.projectContainingActive() === true){
-      if (this.getValue(this.inputFieldName )!== '' && this.getValue(this.inputDate) !== '') {
-        if (this.getValue(this.inputFieldName )!== '' && this.getValue(this.inputDate) !== '') {
-          const name = this.getValue(this.inputFieldName)
-          const description = this.getValue(this.inputFieldDesc)
-          const dueDate = this.getValue(this.inputDate)
-          const priority = this.getValue(this.dropDown)
-          const status = this.checkbox.checked
-  
-  
-          const _task = new Todo(name, description, dueDate, priority, status)
-          // ------------sdfadsffa-----
-          projectStorage.addTaskToProject(trackingActiveClass.getIndexOfActiveTodo(),trackingActiveClass.getButtonIndex(),_task)
-          new DisplayTask(projectStorage.getTaskToProject(trackingActiveClass.getIndexOfActiveTodo(),trackingActiveClass.getButtonIndex())).displayTasks()
-  
+        const _task = new Todo(name, description, dueDate, priority, status)
+        // ------------sdfadsffa-----
+        projectStorage.addTaskToProject(
+          trackingActiveClass.getIndexOfActiveTodo(),
+          trackingActiveClass.getButtonIndex(),
+          _task
+        )
+        this.displayTask(
+          projectStorage.getTaskToProject(
+            trackingActiveClass.getIndexOfActiveTodo(),
+            trackingActiveClass.getButtonIndex()
+          )
+        )
 
-  
-          this.containerDiv.style.display = 'none'
-          addingButton.style.display = 'block'
-          
-        }
-  
+        this.containerDiv.style.display = 'none'
+        addingButton.style.display = 'block'
       }
-      
     }
-
   }
 }
 
-
-
-
-
 // ----
-
-
 
 export class DisplayTask {
   constructor(tasks) {
     this.tasks = tasks
-  }
 
-  displayTasks() {
     const container = document.querySelector('.task') // Assuming you have a <div> element with id 'task-container' in your HTML file.
 
     // Clear the container before displaying the tasks
     container.innerHTML = ''
 
     this.tasks.forEach((task) => {
-      const card = document.createElement('div');
-      card.classList.add('task-card');
+      const card = document.createElement('div')
+      card.classList.add('task-card')
 
-      const nameElement = document.createElement('h3');
-      nameElement.classList.add('task-name');
-      nameElement.textContent = task.name;
+      this.statusElement = document.createElement('input')
+      this.statusElement.setAttribute('type', 'checkbox')
+      this.statusElement.checked = task.status
+      this.statusElement.addEventListener('click', () => {
+        task.status = true
+        if (trackingActiveClass.inboxContainingActive()) {
+          inboxStorage.finishTask()
+        } else if (trackingActiveClass.projectContainingActive()) {
+          projectStorage.finishProjectTask(
+            trackingActiveClass.getIndexOfActiveTodo(),
+            trackingActiveClass.getButtonIndex()
+          )
+        }
+        card.remove()
+      })
 
-      const descriptionElement = document.createElement('p');
-      descriptionElement.classList.add('task-description');
-      descriptionElement.textContent = task.description;
+      this.priorityElement = document.createElement('div')
+      this.priorityElement.classList.add('task-priority')
+      this.priorityElement.classList.add(task.priority.toLowerCase())
 
-      const dueDateElement = document.createElement('p');
-      dueDateElement.classList.add('task-due-date');
-      dueDateElement.textContent = `Due Date: ${task.dueDate}`;
+      this.nameElement = document.createElement('h3')
+      this.nameElement.classList.add('task-name')
+      this.nameElement.textContent = task.name
 
-      const priorityElement = document.createElement('div');
-      priorityElement.classList.add('task-priority');
-      priorityElement.classList.add(task.priority.toLowerCase());
-      
-      const statusElement = document.createElement('input');
-      statusElement.setAttribute('type', 'checkbox');
-      statusElement.checked = task.status;
+      this.descriptionElement = document.createElement('p')
+      this.descriptionElement.classList.add('task-description')
+      this.descriptionElement.textContent = task.description
 
-      card.appendChild(nameElement);
-      card.appendChild(descriptionElement);
-      card.appendChild(dueDateElement);
-      card.appendChild(priorityElement);
-      card.appendChild(statusElement);
+      this.dueDateElement = document.createElement('p')
+      this.dueDateElement.classList.add('task-due-date')
+      this.dueDateElement.textContent = `Due Date: ${task.dueDate}`
+
+      if (task.status === false) {
+        card.appendChild(this.statusElement)
+      }
+      card.appendChild(this.priorityElement)
+      card.appendChild(this.nameElement)
+      card.appendChild(this.descriptionElement)
+      card.appendChild(this.dueDateElement)
 
       container.appendChild(card)
     })
   }
 }
-
-
-
-
-
-
-
-
- 
-
-const addingButton = document.getElementById('adding-button')
-addingButton.addEventListener('click', () =>  {
-   new CreateTaskInput
-    addingButton.style.display = 'none'
-    })
